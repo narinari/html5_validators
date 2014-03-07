3898a8bab0ec7c015ea6654396b5144257c50f43
NARIYAMA Takashi <takashi.nariyama@ulsystems.co.jp>
2014-03-07 17:29:14 +0900

    Add support ActiveModel::Base included classes.

--- lib/html5_validators/active_model/validations.rb
+++ lib/html5_validators/active_model/validations.rb
@@ -1,5 +1,11 @@
 module ActiveModel
   module Validations
     attr_accessor :auto_html5_validation
+    
+    def self.included(kls)
+      kls.class_eval do
+        cattr_accessor :auto_html5_validation, :instance_accessor => false, :instance_reader => false, :instance_writer => false
+      end
+    end
   end
 end
